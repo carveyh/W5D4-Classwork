483bd748d0d23f607bc1232c9be05c9ff7c0028c
carveyh <carvey.hor@gmail.com>
2023-04-06 15:41:49 -0400

    User#enrollments, User#enrolled_courses

--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -8,4 +8,11 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+	has_many :enrollments,
+		class_name: :Enrollment,
+		primary_key: :id,
+		foreign_key: :student_id
+
+	has_many :enrolled_courses,
+		through: :enrollments, source: :course
 end

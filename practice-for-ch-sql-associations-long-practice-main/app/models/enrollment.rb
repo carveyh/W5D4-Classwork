a6e4d6e1c16333758d8935e329580b8de8f7ebf2
jliangrpb <jeremy.liang612@gmail.com>
2023-04-06 15:20:54 -0400

    Finished Enrollment model

--- practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+    belongs_to :course,
+        class_name: :Course,
+        primary_key: :id,
+        foreign_key: :course_id
+
+    belongs_to :student,
+        class_name: :User,
+        primary_key: :id,
+        foreign_key: :student_id
 end

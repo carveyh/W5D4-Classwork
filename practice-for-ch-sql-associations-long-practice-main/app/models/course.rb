061ef975af24165966ee6ebe77b37723ba990ae9
jliangrpb <jeremy.liang612@gmail.com>
2023-04-06 16:49:51 -0400

    Finished everything

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,24 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    # validates :prereq_id, presence: true
+    has_many :enrollments,
+        class_name: :Enrollment,
+        primary_key: :id,
+        foreign_key: :course_id
+
+    has_many :enrolled_students, 
+        through: :enrollments,
+        source: :student 
+
+    belongs_to :prerequisite,
+        class_name: :Course, 
+        primary_key: :id, 
+        foreign_key: :prereq_id,
+        optional: true
+
+    belongs_to :instructor, 
+        class_name: :User,
+        primary_key: :id,
+        foreign_key: :instructor_id
 end
